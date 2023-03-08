808c24c677a8a99a6cc5570ddbef7998f1e22f9b
Crypty28 <marcotilbio@gmail.com>
2023-03-08 08:36:09 -0300

    Aula sobre javascript com variaveis e funções]

---
+++ aula-02/funcoesevariaveis.js
@@ -0,0 +1,17 @@
+var variavel1= "olá"
+let variavel2 = 3
+const variavel3 = 8
+
+console.log(variavel1 + " turminha do " + variavel2+"º"+" ano")
+console.log(variavel2 + variavel3)
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+
+const obj={
+nome:"João",
+signo:"Escorpião"
+
+}
+// Formato JSON - JavaScipt Notation Objectz
+console.log(obj.nome, obj.signo)
+console.log(typeof obj)
