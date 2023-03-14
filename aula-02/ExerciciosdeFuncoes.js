39eb19a87414dc22ce00a5d3be14419d9870a312
Crypty28 <marcotilbio@gmail.com>
2023-03-14 16:56:04 -0300

    Exercicios de função do dia 15/03

---
+++ aula-02/ExerciciosdeFuncoes.js
@@ -0,0 +1,11 @@
+let a = 9
+let b = 2
+
+console.log("\n Considerando A=9 e B=2 então:\n")
+
+console.log("a soma de A e B é:", a+b)
+console.log("a subtração de A e B:",a-b)
+console.log("a divisão de A com B é:", a/b)
+console.log("a multiplicação de A e B é:",a*b)
+console.log("A raiz quadrada de a é:", Math.sqrt(a))
+console.log("A pontencia B de A é:", Math.pow(a, b))
